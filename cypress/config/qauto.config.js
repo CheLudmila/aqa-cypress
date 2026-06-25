a3be25384b9726069ebd41463f107336e9a84f3c
Liudmyla Cheredniuk <Sapphire777.astro@gmail.com>
2026-06-25 20:45:34 +0300

    Configure Cypress Docker GitHub Actions

--- cypress/config/qauto.config.js
+++ cypress/config/qauto.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  baseUrl: "https://qauto.forstudy.space/",
-  email: "Genbroker@gmail.com",
-  password: "Pass1982"
-};
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://qauto.forstudy.space/',
+
+    specPattern: 'e2e/**/*.cy.js',
+
+    supportFile: 'support/e2e.js',
+  },
+
+  fixturesFolder: 'fixtures',
+
+  env: {
+    email: 'Genbroker@gmail.com',
+    password: 'Pass1982',
+  },
+});
--- cypress/config/qauto2.config.js
+++ cypress/config/qauto2.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  baseUrl: "https://qauto2.forstudy.space/",
-  email: "Genbroker2@gmail.com",
-  password: "Pass1982"
-};
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://qauto2.forstudy.space/',
+  },
+
+  env: {
+    email: 'Genbroker2@gmail.com',
+    password: 'Pass1982',
+  },
+});
