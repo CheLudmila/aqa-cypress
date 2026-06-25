149730428d6879f7ec39994a163f71b9ab59c88e
Liudmyla Cheredniuk <Sapphire777.astro@gmail.com>
2026-06-25 21:37:39 +0300

    Fix qauto config

--- cypress/config/qauto.config.js
+++ cypress/config/qauto.config.js
@@ -1,18 +1,5 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://qauto.forstudy.space/',
-
-    specPattern: 'e2e/**/*.cy.js',
-
-    supportFile: 'support/e2e.js',
-  },
-
-  fixturesFolder: 'fixtures',
-
-  env: {
-    email: 'Genbroker@gmail.com',
-    password: 'Pass1982',
-  },
-});
+module.exports = {
+  baseUrl: "https://qauto.forstudy.space/",
+  email: "Genbroker@gmail.com",
+  password: "Pass1982",
+};
